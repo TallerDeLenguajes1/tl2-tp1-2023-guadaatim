1264eaee979d46c747ebcbe6c2659f88acb126c6
guadalupe atim <guadiiatim1309@gmail.com>
2023-08-25 19:21:20 -0300

    clase pedidos

---
+++ Pedidos.cs
@@ -0,0 +1,9 @@
+namespace EspacioPedido;
+using EspacioCliente;
+
+public class Pedido{
+    private int nro;
+    private string? observacion;
+    private Cliente cliente;
+    private string? estado;
+}
