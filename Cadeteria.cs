065d372b0fdfa5974e87fba8b92ed3d27da5e2af
guadalupe atim <guadiiatim1309@gmail.com>
2023-08-25 19:33:08 -0300

    asignar pedido

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -1,9 +1,24 @@
 namespace EspacioCadeteria;
 using EspacioCadete;
+using EspacioPedido;
+using EspacioCliente;
 
 public class EspacioCadeteria
 {
     private string? nombre;
     private string? telefono;
-    private List<Cadete> listadoCadetes;
+    private List<Cadete>? listadoCadetes;
+
+    public string? Nombre { get => nombre; set => nombre = value; }
+    public string? Telefono { get => telefono; set => telefono = value; }
+    public List<Cadete> ListadoCadetes { get => listadoCadetes; set => listadoCadetes = value; }
+
+    //asignar pedido
+
+    public void AsignarPedido(Pedido nuevoPedido, List<Cadete> ListadoCadetes)
+    {
+        
+    }
+
+    //informe 
 }
