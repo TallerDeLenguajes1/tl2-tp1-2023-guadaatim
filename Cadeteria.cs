91ceed7c61ee58c5b4dfdeaadee5428f879eae6c
guadalupe atim <guadiiatim1309@gmail.com>
2023-08-25 19:20:06 -0300

    clase cadeteria

---
+++ Cadeteria.cs
@@ -0,0 +1,9 @@
+namespace EspacioCadeteria;
+using EspacioCadete;
+
+public class EspacioCadeteria
+{
+    private string? nombre;
+    private string? telefono;
+    private List<Cadete> listadoCadetes;
+}
