abd17f0165e90a344cb40a16333380113763de56
guadalupe atim <guadiiatim1309@gmail.com>
2023-08-25 19:18:51 -0300

    clase cadete

---
+++ Cadete.cs
@@ -0,0 +1,10 @@
+namespace EspacioCadete;
+using EspacioPedido;
+
+public class Cadete{
+    private int id;
+    private string? nombre;
+    private string? direccion;
+    private string? telefono;
+    private List<Pedido> listaPedidos;
+}
