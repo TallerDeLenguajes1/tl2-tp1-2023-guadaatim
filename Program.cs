a49b94feec7693c9cb22865577a838a4f11ec760
guadalupe atim <guadiiatim1309@gmail.com>
2023-09-08 19:23:03 -0300

    menu

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@ using EspacioCadeteria;
 using EspacioCliente;
 using EspacioPedido;
 
+Console.WriteLine("BIenvenido!!!");
+
+Console.WriteLine("-------MENU-------");
+Console.WriteLine("1 - Dar de alta un pedido");
+Console.WriteLine("2 - Asignar cadete");
+Console.WriteLine("3 - Cambiar de estado un pedido");
+Console.WriteLine("4 - Reasignar pedido");
+
