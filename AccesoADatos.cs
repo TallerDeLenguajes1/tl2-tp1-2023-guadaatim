8888280f66fbc8c9aab14decad332a71e92a6478
guadalupe atim <guadiiatim1309@gmail.com>
2023-09-27 02:21:30 -0300

    agregando funciones y herencia

--- AccesoADatos.cs
+++ AccesoADatos.cs
@@ -1,8 +1,57 @@
+using EspacioCadete;
+using EspacioCadeteria;
+
 namespace EspacioAccesoADatos;
 
 public class AccesoADatos
 {
+    public List<Cadete> LeerArchivoCadetes(string nombre)
+    {
+        List<Cadete> listadoCadetes = new List<Cadete>();
 
+        StreamReader strCadetes = new StreamReader(nombre);
+        string? linea;
+        int i = 0;
+
+        while ((linea = strCadetes.ReadLine()) != null)
+        {
+            string[] fila = linea.Split(",").ToArray();
+
+            if (i > 0)
+            {
+                Cadete cadeteAgregar = new Cadete(Convert.ToInt32(fila[0]), fila[1], fila[2], fila[3]);
+                listadoCadetes.Add(cadeteAgregar);
+            }
+
+            i++;
+        }
+
+        return listadoCadetes;
+    }
+
+    public List<Cadeteria> LeerArchivoCadeteria(string nombre, List<Cadete> listadoCadetes)
+    {
+        List<Cadeteria> listadoCadeterias = new List<Cadeteria>();
+
+        StreamReader strCadeterias = new StreamReader(nombre);
+        string? linea;
+        int i = 0;
+
+        while ((linea = strCadeterias.ReadLine()) != null)
+        {
+            string[] fila = linea.Split(",").ToArray();
+
+            if (i > 0)
+            {
+                Cadeteria cadeteriaAgregar = new Cadeteria(fila[1], "111", listadoCadetes);
+                listadoCadeterias.Add(cadeteriaAgregar);
+            }
+
+            i++;
+        }
+        
+        return listadoCadeterias;
+    }
 }
 
 public class AccesoCSV : AccesoADatos
@@ -10,7 +59,4 @@ public class AccesoCSV : AccesoADatos
 
 }
 
-public class AccesoJSON : AccesoADatos
-{
-    
-}
+public class AccesoJSON : AccesoADatos;
